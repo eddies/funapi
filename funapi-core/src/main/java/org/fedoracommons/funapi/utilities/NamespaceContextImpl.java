e04e29dcf28d60e518b5e26816bebe2415e112ce
Edwin Shin <edwin.shin@yourmediashelf.com>
2011-03-22 13:55:33 -0700

    add license headers (via maven license plugin)

--- funapi-core/src/main/java/org/fedoracommons/funapi/utilities/NamespaceContextImpl.java
+++ funapi-core/src/main/java/org/fedoracommons/funapi/utilities/NamespaceContextImpl.java
@@ -1,6 +1,20 @@
-/* The contents of this file are subject to the license and copyright terms
- * detailed in the license directory at the root of the source tree (also 
- * available online at http://www.fedora.info/license/).
+/**
+ * Copyright (C) 2008 MediaShelf <http://www.yourmediashelf.com/>
+ *
+ * This file is part of funapi.
+ *
+ * funapi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * funapi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with funapi.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.fedoracommons.funapi.utilities;
 
